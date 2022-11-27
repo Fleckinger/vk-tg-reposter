ee582aac0caf535bf0d4891d70975b2eb0520471
Fleckinger <savarovvld@gmail.com>
2022-11-27 10:31:44 +0300

    initial commit

---
+++ src/main/kotlin/com/fleckinger/vktotgposter/VkToTgPosterApplication.kt
@@ -0,0 +1,11 @@
+package com.fleckinger.vktotgposter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkToTgPosterApplication
+
+fun main(args: Array<String>) {
+    runApplication<VkToTgPosterApplication>(*args)
+}
